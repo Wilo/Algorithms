152f858705bf15b612016951cf55ef5e57383003
William Méndez <williamm@avalith.net>
2023-03-02 21:45:19 -0500

    Doc: implement documentation on package
    Chore: replace argument type using new type alias

--- Python/algorithms/search/binary_search.py
+++ Python/algorithms/search/binary_search.py
@@ -1,10 +1,36 @@
 from helpers import (
     Collection,
-    List,
+    Array,
+    Matrix,
 )
 
 
-def binary_search(collection: Collection, target) -> bool:
+def binary_search(collection: Collection, target: int) -> bool:
+    """Generic Binary Search, you can find and element from an array or 2d Matrix
+
+    Args:
+        collection (List[Union[int, List[int]]]): An Array or 2d Matrix of integers.
+        target (int): Number to find.
+
+    Returns:
+        bool: result if exist or not that element
+
+    Example:
+    >>> matrix = [
+        [1, 2, 4],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    >>> target = 9
+    >>> result = binary_search(matrix, target)
+    >>> print(result)
+    True
+    >>> array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 100, 1000]
+    >>> target = 10000
+    >>> result = binary_search(array, target)
+    >>> print(result)
+    False
+    """
     size: int = len(collection)
     if not size:
         return False  # Empty Collection
@@ -13,7 +39,23 @@ def binary_search(collection: Collection, target) -> bool:
     return binary_search_array(collection, target)
 
 
-def binary_search_array(array: List[int], target: int) -> bool:
+def binary_search_array(array: Array, target: int) -> bool:
+    """A binary search implementation for List of integers
+
+    Args:
+        array (List[int]): List of integers.
+        target (int): Number to find.
+
+    Returns:
+        bool: result if exist or not that element
+
+    Example:
+    >>> array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 100, 1000]
+    >>> target = 9
+    >>> result = binary_search_array(array, target)
+    >>> print(result)
+    True
+    """
     low: int = 0
     high: int = len(array)
     mid: int
@@ -31,7 +73,27 @@ def binary_search_array(array: List[int], target: int) -> bool:
     return False  # Item not found
 
 
-def binary_search_matrix(matrix: List[List[int]], target: int) -> bool:
+def binary_search_matrix(matrix: Matrix, target: int) -> bool:
+    """_summary_
+
+    Args:
+        matrix (List[List[int]]): 2D Matrix of integers.
+        target (int): Number to find.
+
+    Returns:
+        bool: result if exist or not that element
+
+    Example:
+    >>> matrix = [
+        [1, 2, 4],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    >>> target = 9
+    >>> result = binary_search_matrix(matrix, target)
+    >>> print(result)
+    True
+    """
     row_size: int = len(matrix)
     col_size: int = len(matrix[0])
     low: int = 0
